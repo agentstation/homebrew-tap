a631e7554617e435ba0a66331114b85c3b382abf
Jack Spirou <jack@spirou.io>
2026-04-18 16:42:47 +0000

    Update neovex to 0.1.11

--- Casks/neovex.rb
+++ Casks/neovex.rb
@@ -2,7 +2,7 @@ cask "neovex" do
   name "neovex"
   desc "Self-hosted JavaScript backend runtime powered by V8"
   homepage "https://github.com/agentstation/neovex"
-  version "0.1.10"
+  version "0.1.11"
 
   livecheck do
     skip "Auto-generated on release."
@@ -11,30 +11,30 @@ cask "neovex" do
   binary "neovex"
 
   on_macos do
-    on_intel do
-      url "https://github.com/agentstation/neovex/releases/download/v#{version}/neovex_darwin_x86_64.tar.gz"
-      sha256 ""
-    end
+    depends_on arch: :arm64
+    depends_on macos: ">= :sonoma"
+    depends_on formula: "slp/krunkit/krunkit"
+
     on_arm do
       url "https://github.com/agentstation/neovex/releases/download/v#{version}/neovex_darwin_arm64.tar.gz"
-      sha256 "b7190350be58c53644a8243ad93d4b8c509c58a982c9422b7a815185ff55cda5"
+      sha256 "43fcae1dc1ce56876d37b4d52b21501cb5f17ee4ae1fbfb0bf2a064153acbeb8"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/agentstation/neovex/releases/download/v#{version}/neovex_linux_x86_64.tar.gz"
-      sha256 "a9557c2f3feccc9a482331697e18940f47e209423f2f6dc1d8b06e7f45c855c5"
+      sha256 "74249af9c72372c39f12f72a338c00ce3421a50288708996ea7dfc099fe209a8"
     end
     on_arm do
       url "https://github.com/agentstation/neovex/releases/download/v#{version}/neovex_linux_arm64.tar.gz"
-      sha256 "3e50aa487fccb02e9e0f3a9cea774562eb5d1062b1fcb634a2b1d20f15e26e5a"
+      sha256 "76f1528590193650036cfc20c61fe657c0775bad85a807b179442c46eae9511f"
     end
   end
 
   postflight do
     if system_command("/usr/bin/xattr", args: ["-h"]).exit_status == 0
-      system_command "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "#{staged_path}/neovex"], sudo: false
+      system_command "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", staged_path.to_s], sudo: false
     end
   end
 
@@ -43,7 +43,8 @@ cask "neovex" do
     ""
     "Quick start:"
     "  neovex --help              # Show all commands"
-    "  neovex --port 8080         # Start server on port 8080"
+    "  neovex machine init        # Record the default macOS machine contract"
+    "  neovex serve               # Auto-start the machine if needed"
     ""
     "Documentation: https://github.com/agentstation/neovex"
   end
