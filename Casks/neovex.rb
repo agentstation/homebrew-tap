4415109ce5d1dc1c7b140053405bb7f38750915b
Jack Spirou <jack@spirou.io>
2026-04-24 19:15:03 +0000

    Update neovex to 0.1.22

--- Casks/neovex.rb
+++ Casks/neovex.rb
@@ -2,7 +2,7 @@ cask "neovex" do
   name "neovex"
   desc "Self-hosted JavaScript backend runtime powered by V8"
   homepage "https://github.com/agentstation/neovex"
-  version "0.1.20"
+  version "0.1.22"
 
   livecheck do
     skip "Auto-generated on release."
@@ -17,18 +17,18 @@ cask "neovex" do
 
     on_arm do
       url "https://github.com/agentstation/neovex/releases/download/v#{version}/neovex_darwin_arm64.tar.gz"
-      sha256 "b8b543e047b89335804b955632e6a1de139796a6104d533141719d03b57be211"
+      sha256 "9f53ba792f2523905f649b5144ac32157a52ef04c8bae6434a18224a1b586885"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/agentstation/neovex/releases/download/v#{version}/neovex_linux_x86_64.tar.gz"
-      sha256 "a98bbfb354e575050b69b93d4becd15bd3120d8a0f4cc59e603dbc043d8b12a5"
+      sha256 "bfae01c8b2f0d57f5d271b8c61a20934c6a1e51416858d274bab84cc453e335c"
     end
     on_arm do
       url "https://github.com/agentstation/neovex/releases/download/v#{version}/neovex_linux_arm64.tar.gz"
-      sha256 "f5a53e1343a14fc45f8ca9641a4852f6f885ed9a5ce55cddc4ca8f6a5d4db97c"
+      sha256 "1204569d2670767d9f83d295837162a6723520319799134136963d38d4ac272c"
     end
   end
 
